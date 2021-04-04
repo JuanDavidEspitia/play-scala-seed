444fcdaa07ef6b731cb4b5330605ca0c5f69565b
Juan David Espitia <juandespitia14@gmail.com>
2021-04-03 21:16:38 -0500

    Creacion de la clase MovieRepository donde iran las consultas

---
+++ app/models/MovieRepository.scala
@@ -0,0 +1,13 @@
+package models
+
+import javax.inject.Inject
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.api.mvc.{AbstractController, ControllerComponents}
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+ * Clase repository, donde iran las consultas
+ */
+class MovieRepository {
+
+}
